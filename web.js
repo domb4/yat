6bb4f00f6cd79f77dd692dc3e362cdf1f30f349e
domb4 <djiwadomba@gmail.com>
2013-04-10 15:51:24 +0200

    Init

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+var index = fs.readFileSync('index.html', 'utf8');
+
+app.get('/', function(request, response) {
+	response.set('Content-Type', 'text/html');
+	response.send(index);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
